507fbe8e557145f17a4c1d3dadb7a3880965df3a
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2015-10-01 00:00:06 +0200

    handling requestAnimationframe

--- src/snake_cljs/core.cljs
+++ src/snake_cljs/core.cljs
@@ -2,9 +2,18 @@
   (:require [clojure.browser.repl :as repl]
             [goog.dom :as dom]))
 
-(repl/connect "http://localhost:9000/repl")
+;; (repl/connect "http://localhost:9000/repl")
 
 (.log js/console "Hello world!")
 
 (let [drawingContext (.getContext (dom/getElement "canvas") "2d")]
   (.fillRect drawingContext 0 0 10 10))
+
+(def x (atom 0))
+
+(defn animate []
+  (js/requestAnimationFrame animate)
+  (.log js/console (str "Looping... " @x))
+  (swap! x inc))
+
+(animate)
