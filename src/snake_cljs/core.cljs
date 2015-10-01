fb33cf8ce1a5ff7e865a6ba3dec4e9af6cf0863c
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2015-10-02 00:34:38 +0200

    drawing and handling user input

--- src/snake_cljs/core.cljs
+++ src/snake_cljs/core.cljs
@@ -2,18 +2,37 @@
   (:require [clojure.browser.repl :as repl]
             [goog.dom :as dom]))
 
-;; (repl/connect "http://localhost:9000/repl")
+(repl/connect "http://localhost:9000/repl")
 
 (.log js/console "Hello world!")
 
-(let [drawingContext (.getContext (dom/getElement "canvas") "2d")]
-  (.fillRect drawingContext 0 0 10 10))
+(def canvas (dom/getElement "canvas"))
+(def drawingContext (.getContext canvas "2d"))
+(def directions {38 :up, 39 :right, 40 :down, 37 :left})
+(def last-key (atom :right))
+(def i (atom 0))
+(def block-size 10)
+(def dims {:width 20, :height 20})
 
-(def x (atom 0))
+(defn draw-blocks! [blocks]
+  (when-let [block (first blocks)]
+    (.fillRect drawingContext
+               (* block-size (block :x))
+               (* block-size (block :y))
+               block-size
+               block-size)
+    (recur (rest blocks))))
 
-(defn animate []
-  (js/requestAnimationFrame animate)
-  (.log js/console (str "Looping... " @x))
-  (swap! x inc))
+(defn run []
+  (js/requestAnimationFrame run)
+  (.log js/console (str "Looping... " @i " Last key: " @last-key))
+  (swap! i inc))
 
-(animate)
+(set! (.-onkeydown js/window)
+               (fn [e]
+                 (when-let [dir (directions (.-keyCode e))]
+                     (reset! last-key dir))))
+
+(draw-blocks! [{:x 0, :y 0} {:x 1 :y 0}])
+
+(run)
