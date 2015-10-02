def590044012178c15a28db9452707279f6bacb8
Bartłomiej Kruczyk <kruczykb@invicta.pl>
2015-10-02 16:23:24 +0200

    basic movement

--- src/snake_cljs/core.cljs
+++ src/snake_cljs/core.cljs
@@ -12,7 +12,15 @@
 (def last-key (atom :right))
 (def i (atom 0))
 (def block-size 10)
-(def dims {:width 20, :height 20})
+(def dims {:width 50, :height 50})
+(def snake (atom [{:x 4, :y 2} {:x 5, :y 2} {:x 6, :y 2}]))
+(def movements {:up {:x 0, :y -1}, :down {:x 0, :y 1}, :left {:x -1, :y 0}, :right {:x 1 :y 0}})
+
+(defn move [snake dir]
+  (let [movement (movements dir)
+        head (peek snake)]
+    (conj (vec (next snake)) {:x (+ (head :x) (movement :x))
+                              :y (+ (head :y) (movement :y))})))
 
 (defn draw-blocks! [blocks]
   (when-let [block (first blocks)]
@@ -23,9 +31,17 @@
                block-size)
     (recur (rest blocks))))
 
+(defn clear! []
+  (.clearRect drawingContext 0 0 (* block-size (dims :width)) (* block-size (dims :height))))
+
 (defn run []
   (js/requestAnimationFrame run)
   (.log js/console (str "Looping... " @i " Last key: " @last-key))
+  (when (= 0 (mod @i 50))
+    (.log js/console "Render!")
+    (clear!)
+    (reset! snake (move @snake @last-key))
+    (draw-blocks! @snake))
   (swap! i inc))
 
 (set! (.-onkeydown js/window)
@@ -33,6 +49,7 @@
                  (when-let [dir (directions (.-keyCode e))]
                      (reset! last-key dir))))
 
-(draw-blocks! [{:x 0, :y 0} {:x 1 :y 0}])
+(set! (.-width canvas) (* block-size (dims :width)))
+(set! (.-height canvas) (* block-size (dims :height)))
 
 (run)
