d496cac087c3d5d799c3265a8f6781d031413011
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2015-10-15 22:30:15 +0200

    minor refactoring

--- src/snake_cljs/core.cljs
+++ src/snake_cljs/core.cljs
@@ -8,30 +8,34 @@
 
 (def canvas (dom/getElement "canvas"))
 (def drawingContext (.getContext canvas "2d"))
-(def directions {38 :up, 39 :right, 40 :down, 37 :left})
-(def last-key (atom :right))
+
 (def i (atom 0))
+(def last-key (atom :right))
+(def snake (atom [{:x 4, :y 2} {:x 5, :y 2} {:x 6, :y 2}]))
+
 (def block-size 10)
 (def dim 50)
-(def snake (atom [{:x 4, :y 2} {:x 5, :y 2} {:x 6, :y 2}]))
-(def movements {:up {:x 0, :y -1}, :down {:x 0, :y 1}, :left {:x -1, :y 0}, :right {:x 1 :y 0}})
+(def directions {38 :up, 39 :right, 40 :down, 37 :left})
+(def movements {:up {:x 0, :y -1},
+                :down {:x 0, :y 1},
+                :left {:x -1, :y 0},
+                :right {:x 1 :y 0}})
 
-(defn move [snake dir]
-  (let [movement (movements dir)
-        head (peek snake)]
+(defn move [snake movement]
+  "Move snake given as vector points applying to movement."
+  (let [head (peek snake)]
     (conj (vec (next snake)) {:x (+ (head :x) (movement :x))
                               :y (+ (head :y) (movement :y))})))
 
-(defn draw-blocks! [blocks]
-  (when-let [block (first blocks)]
-    (.fillRect drawingContext
-               (* block-size (block :x))
-               (* block-size (block :y))
-               block-size
-               block-size)
-    (recur (rest blocks))))
+(defn grow [snake dir]
+  "Grow snake in given direction."
+  (let [movement (movements dir)
+        head (peek snake)]
+    (conj snake {:x (+ (head :x) (movement :x))
+                 :y (+ (head :y) (movement :y))})))
 
 (defn place-apple [snake dim]
+  "Place apple in a way it doesn't collide with snake."
   (let [scale (.pow js/Math 10 (.ceil js/Math (.log10 js/Math dim)))]
     (loop []
       (let [apple {:x (Math/floor (* (.random js/Math) scale))
@@ -45,36 +49,39 @@
           (recur)
           apple)))))
 
-(def apple (atom (place-apple @snake dim)))
+(defn draw-blocks! [drawingContext blocks]
+  "Draw seq of blocks on given drawing context."
+  (when-let [block (first blocks)]
+    (.fillRect drawingContext
+               (* block-size (block :x))
+               (* block-size (block :y))
+               block-size
+               block-size)
+    (recur drawingContext (rest blocks))))
 
-(defn grow [snake dir]
-  (let [movement (movements dir)
-        head (peek snake)]
-    (conj snake {:x (+ (head :x) (movement :x))
-                 :y (+ (head :y) (movement :y))})))
-
-(defn clear! []
+(defn clear! [drawingContext block-size dim]
+  "Clear drawing context."
   (.clearRect drawingContext 0 0 (* block-size dim) (* block-size dim)))
 
-
+(def apple (atom (place-apple @snake dim)))
 
 (defn run []
   (js/requestAnimationFrame run)
   ;; (.log js/console (str "Looping... " @i " Last key: " @last-key))
   (when (= 0 (mod @i 10))
     (.log js/console "Render!")
-    (clear!)
+    (clear! drawingContext block-size dim)
     (let [new-snake (grow @snake @last-key)]
       (if (some #{@apple} new-snake)
         (do
           (.log js/console "Grow!")
           (reset! snake new-snake)
           (reset! apple (place-apple new-snake dim))))
-      :else (reset! snake (move @snake @last-key)))
+      :else (reset! snake (move @snake (movements @last-key))))
     (set! (.-fillStyle drawingContext) "#000")
-    (draw-blocks! @snake)
+    (draw-blocks! drawingContext @snake)
     (set! (.-fillStyle drawingContext) "#F00")
-    (draw-blocks! [@apple]))
+    (draw-blocks! drawingContext [@apple]))
   (swap! i inc))
 
 (set! (.-onkeydown js/window)
